dcb4e030978cb2142f57c6864536b12b2b437916
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2025-08-26 20:55:08 +0300

    student crud

--- src/main/java/com/abem/project/ProjectApplication.java
+++ src/main/java/com/abem/project/ProjectApplication.java
@@ -3,10 +3,18 @@ package com.abem.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ProjectApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("dbpassword", dotenv.get("dbpassword"));
+		System.setProperty("dbusername", dotenv.get("dbusername"));
+		System.setProperty("dburl", dotenv.get("dburl"));
+
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
